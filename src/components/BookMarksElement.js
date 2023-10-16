ffdc2b02876a075cd068573649d2d8fcbdb80c96
1HIghbarporn <vietcuong12001@gmail.com>
2023-10-16 17:29:29 +0700

    fix bookmark

--- src/components/BookMarksElement.js
+++ src/components/BookMarksElement.js
@@ -1,36 +1,88 @@
-import {Avatar, Box, Stack, Typography, Icon} from '@mui/material';
+import { Avatar, Box, Stack, Typography, Icon, IconButton, MenuItem } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import {File} from 'phosphor-react';
-
-
+import { File } from 'phosphor-react';
+import React, { useState } from 'react';
+import Iconify from './Iconify';
+import MenuPopover from './MenuPopover';
 
 const BookMarkElement = ({ id, name, img, msg }) => {
-    const theme = useTheme();
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                borderRadius: 1,
-                backgroundColor: theme.palette.mode === 'light' ? '#fff' : theme.palette.background.default,
-            }}
-            p={2}
-        >
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Stack direction="row" spacing={2}>
-                    {(
-                        <Box overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} variant="dot">
-                            <File src={img} size={32} />
-                        </Box>
-                    )}
+  const [open, setOpen] = useState(null);
+  const ICON = {
+    mr: 2,
+    width: 20,
+    height: 20,
+  };
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
 
-                    <Stack spacing={0.3}>
-                        <Typography variant="subtitle2">{name}</Typography>
-                        <Typography variant="caption">{msg  }</Typography>
-                    </Stack>
-                </Stack>
-            </Stack>
-        </Box>
-    );
+  const handleClose = () => {
+    setOpen(null);
+  };
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        borderRadius: 1,
+        backgroundColor: theme.palette.mode === 'light' ? '#fff' : theme.palette.background.default,
+      }}
+      p={2}
+    >
+      <Stack direction="row">
+        <Stack direction="row" spacing={2}>
+          {
+            <Box
+              overlap="circular"
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              variant="dot"
+              align="left"
+            >
+              <File src={img} size={32} />
+            </Box>
+          }
+
+          <Stack spacing={0.3}>
+            <Typography variant="subtitle2">{name}</Typography>
+            <Typography variant="caption">{msg}</Typography>
+          </Stack>
+          <IconButton align="right" size="large" color="inherit" onClick={handleOpen}>
+            <Iconify icon={'eva:more-vertical-fill'} width={20} height={20} />
+          </IconButton>
+          <MenuPopover
+            open={Boolean(open)}
+            anchorEl={open}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            arrow="right-top"
+            sx={{
+              mt: -0.5,
+              width: 'auto',
+              '& .MuiMenuItem-root': { px: 1, typography: 'body2', borderRadius: 0.75 },
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Iconify icon={'fluent-emoji-high-contrast:open-file-folder'} sx={{ ...ICON }} />
+              Mở
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              <Iconify icon={'eva:edit-fill'} sx={{ ...ICON }} />
+              Chỉnh sửa
+            </MenuItem>
+            <MenuItem onClick={handleClose} sx={{ color: 'error.main' }}>
+              <Iconify icon={'eva:trash-2-outline'} sx={{ ...ICON }} />
+              Xoá
+            </MenuItem>
+          </MenuPopover>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 };
 
 export default BookMarkElement;
--- src/pages/dashboard/BookMark.jsx
+++ src/pages/dashboard/BookMark.jsx
@@ -1,26 +1,10 @@
-import { Box, IconButton, Stack, Typography, Button, Divider, MenuItem, Icon } from '@mui/material';
-import React, { useState } from 'react';
-import { ArchiveBox, CircleDashed, MagnifyingGlass } from 'phosphor-react';
+import { Box, Stack, Typography } from '@mui/material';
+import React from 'react';
 import { useTheme } from '@mui/material/styles';
-import Iconify from '../../components/Iconify';
-import MenuPopover from '../../components/MenuPopover';
 import { bookmarkList } from '../../_apis_/data';
 import BookMarkElement from '../../components/BookMarksElement';
 
 export default function BookMark() {
-  const [open, setOpen] = useState(null);
-  const ICON = {
-    mr: 2,
-    width: 20,
-    height: 20,
-  };
-  const handleOpen = (event) => {
-    setOpen(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setOpen(null);
-  };
   const theme = useTheme();
   return (
     <>
@@ -44,38 +28,6 @@ export default function BookMark() {
               {bookmarkList.map((el) => (
                 <BookMarkElement key={el.id} {...el} />
               ))}
-              <>
-                <IconButton
-                  size="large"
-                  color="inherit"
-                  sx={{ position: 'absolute', top: '5px', right: '5px', opacity: 0.99 }}
-                  onClick={handleOpen}
-                >
-                  <Iconify icon={'eva:more-vertical-fill'} width={20} height={20} />
-                </IconButton>
-                <MenuPopover
-                  open={Boolean(open)}
-                  anchorEl={open}
-                  onClose={handleClose}
-                  anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-                  transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-                  arrow="right-top"
-                  sx={{
-                    mt: -0.5,
-                    width: 'auto',
-                    '& .MuiMenuItem-root': { px: 1, typography: 'body2', borderRadius: 0.75 },
-                  }}
-                >
-                  <MenuItem
-                    onClick={() => {
-                      handleClose();
-                    }}
-                  >
-                    <Iconify icon={'eva:edit-fill'} sx={{ ...ICON }} />
-                    Chỉnh sửa
-                  </MenuItem>
-                </MenuPopover>
-              </>
             </Stack>
           </Stack>
         </Stack>
